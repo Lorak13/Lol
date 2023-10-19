b90117bda9f72c042817a9622396ceec83ad1adf
Lorak13 <130525191+Lorak13@users.noreply.github.com>
2023-10-19 19:31:51 +0200

    Update Lol_Predictions.py

--- Lol_Predictions.py
+++ Lol_Predictions.py
@@ -3,7 +3,7 @@ import random
 from collections import defaultdict
 
 
-def run_simulation(iterations, team_strengths):
+def run_simulation(iterations, team_strengths, debug_mode):
     record_counter = defaultdict(lambda: defaultdict(int))
     debug_info = []  # Initialize a list to store debug info
 
